eac7012279c35570ded830aca52092ca96b26629
KlevetA <60343282+KlevetA@users.noreply.github.com>
2020-02-12 08:48:37 +0300

    Update Osnov.java
    Результат в 1 строку вместо 3

--- Osnov.java
+++ Osnov.java
@@ -25,30 +25,41 @@ public class Osnov {
 
         String[] subStr;
         subStr = qq.split(delimeter);
+
+
+
+
+
 //разбили на массив subStr, пробел разделитель
         for(int i = 0; i < subStr.length; i++) {
-           // System.out.println(subStr[i]);
+
+
+
+
+
+
+            // System.out.println(subStr[i]);
+
 //проверяем числа там или буквы
             if (checkString(subStr[0]) && checkString(subStr[2])){
-                    //значит в обоих случаях это int
-                   // System.out.println("значит в обоих случаях это int");
+                //значит в обоих случаях это int
+                // System.out.println("значит в обоих случаях это int");
 
 
 
 
 
-
-             //тут надо проверить 3 и 6
-                 if (error.CheckErrors.check36(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2]))){
-                     System.out.println("Исключение по п.3.6           не пдоходящие входные числа");
-                     break;
-                 }
-             //тут надо проверить 7
+                //тут надо проверить 3 и 6
+                if (error.CheckErrors.check36(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2]))){
+                    System.out.println("Исключение по п.3.6           не пдоходящие входные числа");
+                    break;
+                }
+                //тут надо проверить 7
                 if (error.CheckErrors.check7(subStr[1])){
                     System.out.println("Исключение по п.7           калькулятор такого не умеет");
                     break;
                 }
-             //тут надо проверить 4
+                //тут надо проверить 4
                 // ---------------------------четне работает
 
 
@@ -58,43 +69,10 @@ public class Osnov {
                 }
 
 
-                    if (subStr[1].equals("+")) {
-                        System.out.println(calc.Calculation.plus(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2])));
-                        } else {
-                            if (subStr[1].equals("-")) {
-                                System.out.println(calc.Calculation.minus(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2])));
-                            } else {
-                                if (subStr[1].equals("*")) {
-                                    System.out.println(calc.Calculation.multiply(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2])));
-                                } else if (subStr[1].equals("/")) {
-                                    System.out.println(calc.Calculation.division(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2])));
-                                }
-                            }
-                        }
+
             }
             else {
                 if (!checkString(subStr[0]) && !checkString(subStr[2])) {
-                //значит в обоих случаях string
-                    //System.out.println("значит в обоих случаях string");
-
-                    if (subStr[1].equals("+")) {
-                        System.out.println( roman.RomaCalc.convert( calc.Calculation.plus(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]))));
-
-                    } else {
-                        if (subStr[1].equals("-")) {
-                            //calc.Calculation.minus(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]));
-                            System.out.println( roman.RomaCalc.convert( calc.Calculation.minus(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]))));
-                        } else {
-                            if (subStr[1].equals("*")) {
-                                //calc.Calculation.multiply(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]));
-                                System.out.println( roman.RomaCalc.convert( calc.Calculation.multiply(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]))));
-                            } else if (subStr[1].equals("/")) {
-                                //calc.Calculation.division(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]));
-                                System.out.println(roman.RomaCalc.convert(calc.Calculation.division(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]))));
-                            }
-                        }
-                    }
-                   // arabic.AraCalc.AraRoma(subStr[0]);
                 }
                 else {
                     System.out.println("исключение по п.5           одновременно пытаются и арабские и римские...");
@@ -102,9 +80,68 @@ public class Osnov {
             }
         }
 
+//это по числам
+        if (checkString(subStr[0]) && checkString(subStr[2])) {
+            if (subStr[1].equals("+")) {
+                System.out.println(calc.Calculation.plus(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2])));
+            } else {
+                if (subStr[1].equals("-")) {
+                    System.out.println(calc.Calculation.minus(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2])));
+                } else {
+                    if (subStr[1].equals("*")) {
+                        System.out.println(calc.Calculation.multiply(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2])));
+                    } else if (subStr[1].equals("/")) {
+                        System.out.println(calc.Calculation.division(Integer.parseInt(subStr[0]), Integer.parseInt(subStr[2])));
+                    }
+                }
+            }
+        }
+//это если буквы
+        else {
+            if (!checkString(subStr[0]) && !checkString(subStr[2])) {
+
+                //значит в обоих случаях string
+                //System.out.println("значит в обоих случаях string");
+
+                if (subStr[1].equals("+")) {
+                    System.out.println(roman.RomaCalc.convert(calc.Calculation.plus(arabic.AraCalc.AraRoma(subStr[0]), arabic.AraCalc.AraRoma(subStr[2]))));
+
+                    //------------------------
+                    //System.out.println("PRTL ++++ текст");
+
+                } else {
+                    if (subStr[1].equals("-")) {
+                        //calc.Calculation.minus(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]));
+                        System.out.println(roman.RomaCalc.convert(calc.Calculation.minus(arabic.AraCalc.AraRoma(subStr[0]), arabic.AraCalc.AraRoma(subStr[2]))));
+                        //------------------------
+                        //System.out.println("PRTL ----текст");
+                    } else {
+                        if (subStr[1].equals("*")) {
+                            //calc.Calculation.multiply(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]));
+                            System.out.println(roman.RomaCalc.convert(calc.Calculation.multiply(arabic.AraCalc.AraRoma(subStr[0]), arabic.AraCalc.AraRoma(subStr[2]))));
+                            //------------------------
+                            //System.out.println("PRTL ******текст");
+                        } else if (subStr[1].equals("/")) {
+                            //calc.Calculation.division(arabic.AraCalc.AraRoma(subStr[0]),arabic.AraCalc.AraRoma(subStr[2]));
+                            System.out.println(roman.RomaCalc.convert(calc.Calculation.division(arabic.AraCalc.AraRoma(subStr[0]), arabic.AraCalc.AraRoma(subStr[2]))));
+                            //------------------------
+                            //System.out.println("PRTL ////текст");
+                        }
+                    }
+                }
+                // arabic.AraCalc.AraRoma(subStr[0]);
+            }
+        }
 
 
 
+
+
+
+       // System.out.println(subStr[0]);
+       // System.out.println(subStr[1]);
+       // System.out.println(subStr[2]);
+       // System.out.println(subStr[3]);
 
     }
 
